f2646839748b4db273afb9da41a1b1c4c3818136
Kevin Chiu <kevin.chiu@bandlab.com>
2022-09-17 15:11:46 +0800

    Version bump v1.1.3

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -6,7 +6,7 @@ object AppConfig {
     const val APPLICATION_ID = "com.kevlina.budgetplus"
 
     // Budget+ version
-    const val APP_VERSION = "1.1.2"
+    const val APP_VERSION = "1.1.3"
 
     // Minimal Android sdk version
     const val MIN_ANDROID_SDK_VERSION = 23

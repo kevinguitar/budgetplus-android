41bc8864a7b2fe777746faded8bbec85161eb12d
Kevin Chiu <kevin85619@gmail.com>
2023-02-16 20:57:47 +0800

    Do not use material button

--- core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/AppButton.kt
+++ core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/AppButton.kt
@@ -1,10 +1,15 @@
 package com.kevlina.budgetplus.core.ui
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -17,18 +22,25 @@ fun AppButton(
     enabled: Boolean = true,
     color: Color = LocalAppColors.current.dark,
     shape: Shape = RoundedCornerShape(12.dp),
-    content: @Composable RowScope.() -> Unit
-) = Button(
-    modifier = modifier,
-    onClick = onClick,
-    enabled = enabled,
-    colors = ButtonDefaults.buttonColors(
-        containerColor = color,
-        contentColor = LocalAppColors.current.light,
-        disabledContainerColor = color.copy(alpha = 0.4F),
-        disabledContentColor = LocalAppColors.current.light
-    ),
-    elevation = null,
-    shape = shape,
-    content = content
-)
+    content: @Composable RowScope.() -> Unit,
+) {
+    Surface(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        shape = shape,
+        color = if (enabled) {
+            color
+        } else {
+            color.copy(alpha = 0.4F)
+        },
+        interactionSource = remember { MutableInteractionSource() }
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            content = content
+        )
+    }
+}

71731ea505b441633b87b1557d317326546a498f
Kevin Chiu <kevin85619@gmail.com>
2023-03-02 22:34:57 +0800

    Update the issue link regarding desugaring

--- core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/DatePickerDialog.kt
+++ core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/DatePickerDialog.kt
@@ -25,86 +25,6 @@ import com.kevlina.budgetplus.core.common.R
 import java.time.LocalDate
 import java.util.concurrent.TimeUnit
 
-// TODO: We may revert to Material DatePicker until this issue is fixed
-// https://issuetracker.google.com/issues/269789430
-
-/*
-@Composable
-fun DatePickerDialog(
-    date: LocalDate,
-    minDate: LocalDate? = null,
-    maxDate: LocalDate? = null,
-    onDatePicked: (LocalDate) -> Unit,
-    onDismiss: () -> Unit,
-) {
-
-    val dateState = rememberDatePickerState(initialSelectedDateMillis = date.utcMillis)
-    val colors = DatePickerDefaults.colors(
-        containerColor = LocalAppColors.current.light,
-        currentYearContentColor = LocalAppColors.current.primarySemiDark,
-        selectedYearContentColor = LocalAppColors.current.light,
-        selectedYearContainerColor = LocalAppColors.current.primarySemiDark,
-        selectedDayContainerColor = LocalAppColors.current.primarySemiDark,
-        todayContentColor = LocalAppColors.current.primarySemiDark,
-        todayDateBorderColor = LocalAppColors.current.primarySemiDark,
-    )
-
-    MaterialDatePickerDialog(
-        onDismissRequest = onDismiss,
-        colors = colors,
-        dismissButton = {
-            AppText(
-                text = stringResource(id = R.string.cta_cancel),
-                color = Color.Black,
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier
-                    .clip(shape = RoundedCornerShape(8.dp))
-                    .rippleClick(onClick = onDismiss)
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-            )
-        },
-        confirmButton = {
-            AppText(
-                text = stringResource(id = R.string.cta_ok),
-                color = Color.Black,
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier
-                    .padding(end = 16.dp)
-                    .clip(shape = RoundedCornerShape(8.dp))
-                    .rippleClick {
-                        onDatePicked(dateState.selectedDateMillis?.utcLocaleDate ?: date)
-                        onDismiss()
-                    }
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-            )
-        }
-    ) {
-
-        DatePicker(
-            state = dateState,
-            colors = colors,
-            showModeToggle = false,
-            title = {
-                AppText(
-                    text = stringResource(id = R.string.select_date),
-                    color = Color.Black
-                )
-            },
-            dateValidator = { millis ->
-                when {
-                    minDate != null && maxDate != null -> {
-                        millis >= minDate.utcMillis && millis <= maxDate.utcMillis
-                    }
-
-                    minDate != null -> millis >= minDate.utcMillis
-                    maxDate != null -> millis <= maxDate.utcMillis
-                    else -> true
-                }
-            }
-        )
-    }
-}*/
-
 //TODO: LocalDate type is unstable
 @Composable
 fun DatePickerDialog(
@@ -202,3 +122,100 @@ private fun CustomDatePicker(
         }
     )
 }
+
+// TODO Cannot use the new style because of this bug from desugaring library
+// see: https://issuetracker.google.com/issues/176502609
+/*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.kevlina.budgetplus.core.common.R
+import com.kevlina.budgetplus.core.common.utcLocaleDate
+import com.kevlina.budgetplus.core.common.utcMillis
+import java.time.LocalDate
+import androidx.compose.material3.DatePickerDialog as MaterialDatePickerDialog
+
+@Composable
+fun DatePickerDialog(
+    date: LocalDate,
+    minDate: LocalDate? = null,
+    maxDate: LocalDate? = null,
+    onDatePicked: (LocalDate) -> Unit,
+    onDismiss: () -> Unit,
+) {
+
+    val dateState = rememberDatePickerState(initialSelectedDateMillis = date.utcMillis)
+    val colors = DatePickerDefaults.colors(
+        containerColor = LocalAppColors.current.light,
+        currentYearContentColor = LocalAppColors.current.primarySemiDark,
+        selectedYearContentColor = LocalAppColors.current.light,
+        selectedYearContainerColor = LocalAppColors.current.primarySemiDark,
+        selectedDayContainerColor = LocalAppColors.current.primarySemiDark,
+        todayContentColor = LocalAppColors.current.primarySemiDark,
+        todayDateBorderColor = LocalAppColors.current.primarySemiDark,
+    )
+
+    MaterialDatePickerDialog(
+        onDismissRequest = onDismiss,
+        colors = colors,
+        dismissButton = {
+            AppText(
+                text = stringResource(id = R.string.cta_cancel),
+                color = Color.Black,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(8.dp))
+                    .rippleClick(onClick = onDismiss)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+        },
+        confirmButton = {
+            AppText(
+                text = stringResource(id = R.string.cta_ok),
+                color = Color.Black,
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .clip(shape = RoundedCornerShape(8.dp))
+                    .rippleClick {
+                        onDatePicked(dateState.selectedDateMillis?.utcLocaleDate ?: date)
+                        onDismiss()
+                    }
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+        }
+    ) {
+
+        DatePicker(
+            state = dateState,
+            colors = colors,
+            showModeToggle = false,
+            title = {
+                AppText(
+                    text = stringResource(id = R.string.select_date),
+                    color = Color.Black
+                )
+            },
+            dateValidator = { millis ->
+                when {
+                    minDate != null && maxDate != null -> {
+                        millis >= minDate.utcMillis && millis <= maxDate.utcMillis
+                    }
+
+                    minDate != null -> millis >= minDate.utcMillis
+                    maxDate != null -> millis <= maxDate.utcMillis
+                    else -> true
+                }
+            }
+        )
+    }
+}*/

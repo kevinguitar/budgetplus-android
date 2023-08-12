f6145da07d57882ee9bac38590c823dc5370b08c
Kevin Chiu <kevin85619@gmail.com>
2023-08-12 15:25:19 +0800

    Update the baseline-prof.txt

--- benchmark/src/main/kotlin/com/kevlina/budgetplus/benchmark/Utils.kt
+++ benchmark/src/main/kotlin/com/kevlina/budgetplus/benchmark/Utils.kt
@@ -5,12 +5,25 @@ import androidx.test.uiautomator.By
 import androidx.test.uiautomator.Until
 
 internal const val APP_PACKAGE = "com.kevlina.budgetplus"
-private const val UI_TIMEOUT = 3_000L
+private const val UI_TIMEOUT = 1_000L
 
 internal fun MacrobenchmarkScope.authorize() {
-    val isVisible = device.wait(Until.hasObject(By.text("Continue as Kevin")), UI_TIMEOUT)
+    if (!hasText("Welcome to Budget+")) {
+        return
+    }
+
+    waitForTextAndClick("Continue with Google")
+    waitForTextAndClick("kevin.chiu@bandlab.com")
+    hasText("Expense")
+}
+
+private fun MacrobenchmarkScope.hasText(text: String): Boolean {
+    return device.wait(Until.hasObject(By.text(text)), UI_TIMEOUT)
+}
+
+private fun MacrobenchmarkScope.waitForTextAndClick(text: String) {
+    val isVisible = device.wait(Until.hasObject(By.text(text)), UI_TIMEOUT)
     if (isVisible) {
-        device.findObject(By.text("Continue as Kevin")).click()
-        device.wait(Until.hasObject(By.text("Expense")), UI_TIMEOUT)
+        device.findObject(By.text(text)).click()
     }
 }

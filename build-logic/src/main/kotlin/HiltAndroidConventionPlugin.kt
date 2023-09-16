884fbf8cd20276ee22e22ded81296f94302a7d5a
Kevin Chiu <kevin85619@gmail.com>
2023-09-16 11:56:57 +0800

    Hilt ksp!

--- build-logic/src/main/kotlin/HiltAndroidConventionPlugin.kt
+++ build-logic/src/main/kotlin/HiltAndroidConventionPlugin.kt
@@ -1,33 +1,21 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class HiltAndroidConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         pluginManager.apply {
             apply("com.google.dagger.hilt.android")
-            apply("org.jetbrains.kotlin.kapt")
-        }
-
-        extensions.configure<KaptExtension> {
-            correctErrorTypes = true
-            // This is experimental feature but it can improve build time
-            // If you faced issue with annotation processing please report to http://kotl.in/issue
-            useBuildCache = true
-            // If you get error during annotation processing you will see link to
-            // original Kotlin file instead of generated Java stub
-            mapDiagnosticLocations = true
+            apply("com.google.devtools.ksp")
         }
 
         val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
         dependencies {
             add("implementation", libs.findLibrary("navigation.hilt").get())
             add("implementation", libs.findLibrary("hilt.android").get())
-            add("kapt", libs.findLibrary("hilt.compiler").get())
+            add("ksp", libs.findLibrary("hilt.compiler").get())
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.google.services) apply false

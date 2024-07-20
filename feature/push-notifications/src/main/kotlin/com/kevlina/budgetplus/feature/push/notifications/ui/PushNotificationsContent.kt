6c765ff452b89a57ffa0904a3d512be76c8aba88
Kevin Chiu <kevin85619@gmail.com>
2024-07-20 08:28:48 +0800

    Add imePadding to PushNotifications screen

--- feature/push-notifications/src/main/kotlin/com/kevlina/budgetplus/feature/push/notifications/ui/PushNotificationsContent.kt
+++ feature/push-notifications/src/main/kotlin/com/kevlina/budgetplus/feature/push/notifications/ui/PushNotificationsContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -59,6 +60,7 @@ internal fun PushNotificationsContent(
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
             .padding(16.dp)
+            .imePadding()
     ) {
 
         LanguageBlock(

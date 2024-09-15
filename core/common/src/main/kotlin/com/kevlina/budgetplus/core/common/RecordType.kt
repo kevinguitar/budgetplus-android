488e0723550686ae4e5f6b5aa8ac90ab59a14ec7
Kevin Chiu <kevin85619@gmail.com>
2024-09-15 13:00:59 +0800

    Fix a navigation crash with minified build

--- core/common/src/main/kotlin/com/kevlina/budgetplus/core/common/RecordType.kt
+++ core/common/src/main/kotlin/com/kevlina/budgetplus/core/common/RecordType.kt
@@ -1,5 +1,9 @@
 package com.kevlina.budgetplus.core.common
 
+import androidx.annotation.Keep
+
+// https://issuetracker.google.com/issues/358687142
+@Keep
 enum class RecordType {
     Expense, Income
 }

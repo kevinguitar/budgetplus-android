58435311f8ad735c1e0d29a5a46f2094bd4fdfa3
Kevin Chiu <kevin85619@gmail.com>
2024-12-20 20:26:11 +0800

    Use Material3 DatePickerDialog

--- core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/DatePickerDialog.kt
+++ core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/DatePickerDialog.kt
@@ -1,129 +1,5 @@
 package com.kevlina.budgetplus.core.ui
 
-import android.widget.DatePicker
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.window.Dialog
-import com.kevlina.budgetplus.core.common.R
-import java.time.LocalDate
-import java.util.concurrent.TimeUnit
-
-@Composable
-fun DatePickerDialog(
-    date: LocalDate,
-    minDate: LocalDate? = null,
-    maxDate: LocalDate? = null,
-    onDatePicked: (LocalDate) -> Unit,
-    onDismiss: () -> Unit,
-) {
-
-    var currentDate by remember { mutableStateOf(date) }
-
-    Dialog(onDismissRequest = onDismiss) {
-
-        Column(
-            modifier = Modifier
-                .background(
-                    color = Color.DarkGray,
-                    shape = AppTheme.dialogShape
-                )
-                .clip(AppTheme.dialogShape)
-        ) {
-
-            CustomDatePicker(
-                date = currentDate,
-                minDate = minDate,
-                maxDate = maxDate,
-                onDateSelected = { currentDate = it }
-            )
-
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(bottom = 16.dp, end = 16.dp)
-            ) {
-
-                Text(
-                    text = stringResource(id = R.string.cta_cancel),
-                    color = Color.White,
-                    modifier = Modifier
-                        .clip(shape = RoundedCornerShape(8.dp))
-                        .rippleClick(onClick = onDismiss)
-                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                )
-
-                Text(
-                    text = stringResource(id = R.string.cta_confirm),
-                    color = Color.White,
-                    modifier = Modifier
-                        .clip(shape = RoundedCornerShape(8.dp))
-                        .rippleClick {
-                            onDatePicked(currentDate)
-                            onDismiss()
-                        }
-                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                )
-            }
-        }
-    }
-}
-
-@Composable
-private fun CustomDatePicker(
-    date: LocalDate,
-    minDate: LocalDate?,
-    maxDate: LocalDate?,
-    onDateSelected: (LocalDate) -> Unit,
-) {
-    AndroidView(
-        modifier = Modifier.wrapContentSize(),
-        factory = { context -> DatePicker(context) },
-        update = { view ->
-            view.init(
-                /* year = */ date.year,
-                /* monthOfYear = */ date.monthValue - 1,
-                /* dayOfMonth = */ date.dayOfMonth
-            ) { _, year, monthOfYear, dayOfMonth ->
-                onDateSelected(
-                    LocalDate.now()
-                        .withMonth(monthOfYear + 1)
-                        .withYear(year)
-                        .withDayOfMonth(dayOfMonth)
-                )
-            }
-
-            if (minDate != null) {
-                view.minDate = minDate.toEpochDay() * TimeUnit.DAYS.toMillis(1)
-            }
-
-            if (maxDate != null) {
-                view.maxDate = maxDate.toEpochDay() * TimeUnit.DAYS.toMillis(1)
-            }
-        }
-    )
-}
-
-// Cannot use the new style because of this bug from desugaring library
-// see: https://issuetracker.google.com/issues/300128109
-/*
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DatePicker
@@ -139,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kevlina.budgetplus.core.common.R
 import com.kevlina.budgetplus.core.theme.LocalAppColors
+import com.kevlina.budgetplus.core.theme.ThemeColors
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
@@ -169,14 +46,34 @@ fun DatePickerDialog(
         }
     )
 
+    val lightColor = LocalAppColors.current.light
+    val primaryColor = LocalAppColors.current.primary
+    val darkColor = LocalAppColors.current.dark
     val colors = DatePickerDefaults.colors(
-        containerColor = LocalAppColors.current.light,
-        currentYearContentColor = LocalAppColors.current.dark,
-        selectedYearContentColor = LocalAppColors.current.light,
-        selectedYearContainerColor = LocalAppColors.current.dark,
-        selectedDayContainerColor = LocalAppColors.current.dark,
-        todayContentColor = LocalAppColors.current.dark,
-        todayDateBorderColor = LocalAppColors.current.dark,
+        containerColor = lightColor,
+        titleContentColor = darkColor,
+        headlineContentColor = darkColor,
+        weekdayContentColor = darkColor,
+        subheadContentColor = darkColor,
+        navigationContentColor = darkColor,
+        yearContentColor = darkColor,
+        disabledYearContentColor = darkColor.copy(alpha = DisabledAlpha),
+        currentYearContentColor = darkColor,
+        selectedYearContentColor = lightColor,
+        disabledSelectedYearContentColor = primaryColor.copy(alpha = DisabledAlpha),
+        selectedYearContainerColor = primaryColor,
+        disabledSelectedYearContainerColor = darkColor.copy(alpha = DisabledAlpha),
+        dayContentColor = darkColor,
+        disabledDayContentColor = darkColor.copy(alpha = DisabledAlpha),
+        selectedDayContentColor = lightColor,
+        disabledSelectedDayContentColor = lightColor.copy(alpha = DisabledAlpha),
+        selectedDayContainerColor = primaryColor,
+        disabledSelectedDayContainerColor = primaryColor.copy(alpha = DisabledAlpha),
+        todayContentColor = darkColor,
+        todayDateBorderColor = primaryColor,
+        dayInSelectionRangeContentColor = darkColor,
+        dayInSelectionRangeContainerColor = darkColor,
+        dividerColor = darkColor,
     )
 
     MaterialDatePickerDialog(
@@ -190,7 +87,7 @@ fun DatePickerDialog(
                 modifier = Modifier
                     .clip(shape = RoundedCornerShape(8.dp))
                     .rippleClick(onClick = onDismiss)
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .padding(all = 16.dp)
             )
         },
         confirmButton = {
@@ -205,25 +102,19 @@ fun DatePickerDialog(
                         onDatePicked(dateState.selectedDateMillis?.utcLocaleDate ?: date)
                         onDismiss()
                     }
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .padding(all = 16.dp)
             )
         }
     ) {
-
         DatePicker(
             state = dateState,
             colors = colors,
-            showModeToggle = false,
-            title = {
-                Text(
-                    text = stringResource(id = R.string.select_date),
-                    color = LocalAppColors.current.dark,
-                    modifier = Modifier.padding(16.dp)
-                )
-            }
+            showModeToggle = false
         )
     }
 }
+
+private const val DisabledAlpha = 0.38f
 
 private val LocalDate.utcMillis
     get() = atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli()
@@ -233,11 +124,12 @@ private val Long.utcLocaleDate: LocalDate
 
 @Preview
 @Composable
-private fun DatePickerDialog_Preview() = AppTheme {
+private fun DatePickerDialog_Preview() = AppTheme(ThemeColors.Barbie) {
     DatePickerDialog(
         date = LocalDate.now(),
+        minDate = LocalDate.now().minusWeeks(1),
+        maxDate = LocalDate.now().plusWeeks(1),
         onDismiss = {},
         onDatePicked = {}
     )
 }
-*/

7ebadd6ee509bee047abf54989af67873d0fcd27
Kevin Chiu <kevin85619@gmail.com>
2025-04-27 16:43:44 +0800

    reactive search result

--- feature/search/src/main/kotlin/com/kevlina/budgetplus/feature/search/SearchRepo.kt
+++ feature/search/src/main/kotlin/com/kevlina/budgetplus/feature/search/SearchRepo.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.toObject
 import com.kevlina.budgetplus.core.common.SnackbarSender
@@ -12,17 +13,17 @@ import com.kevlina.budgetplus.core.data.BookRepo
 import com.kevlina.budgetplus.core.data.remote.BooksDb
 import com.kevlina.budgetplus.core.data.remote.Record
 import com.kevlina.budgetplus.feature.search.ui.SearchPeriod
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.shareIn
-import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import java.time.LocalDate
 import javax.inject.Inject
@@ -45,42 +46,51 @@ class SearchRepo @Inject constructor(
         .flatMapLatest { (query, period) ->
             val bookId = bookRepo.currentBookId
             if (query.isBlank() || bookId.isNullOrEmpty()) {
-                return@flatMapLatest flowOf(DbResult.Empty)
-            }
-
-            flow<DbResult> {
-                emit(DbResult.Loading)
-                Timber.d("Search: Performing DB query with period $period")
-                val records = try {
-                    booksDb.get()
-                        .document(bookId)
-                        .collection("records")
-                        .orderBy("date", Query.Direction.DESCENDING)
-                        .whereGreaterThanOrEqualTo("date", period.fromDate().toEpochDay())
-                        .apply {
-                            val untilDate = period.untilDate()
-                            if (untilDate != null) {
-                                whereLessThanOrEqualTo("date", untilDate.toEpochDay())
-                            }
-                        }
-                        .get()
-                        .await()
-                        .map { doc -> doc.toObject<Record>().copy(id = doc.id) }
-                } catch (e: Exception) {
-                    snackbarSender.sendError(e)
-                    emptyList()
-                }
-                Timber.d("Search: result size ${records.size}")
-
-                if (records.isEmpty()) {
-                    emit(DbResult.Empty)
-                } else {
-                    emit(DbResult.Success(records))
-                }
+                flowOf(DbResult.Empty)
+            } else {
+                queryFromDb(bookId, period)
             }
         }
         // Reply the latest result to avoid querying DB again
         .shareIn(viewModelScope, SharingStarted.Lazily, replay = 1)
+
+    private var recordsRegistration: ListenerRegistration? = null
+
+    private fun queryFromDb(
+        bookId: String,
+        period: SearchPeriod,
+    ): Flow<DbResult> {
+        val flow = MutableSharedFlow<DbResult>(
+            onBufferOverflow = BufferOverflow.DROP_OLDEST,
+            replay = 1
+        )
+
+        Timber.d("Search: Performing DB query with period $period")
+        flow.tryEmit(DbResult.Loading)
+
+        recordsRegistration?.remove()
+        recordsRegistration = booksDb.get()
+            .document(bookId)
+            .collection("records")
+            .orderBy("date", Query.Direction.DESCENDING)
+            .whereGreaterThanOrEqualTo("date", period.fromDate().toEpochDay())
+            .whereLessThanOrEqualTo("date", period.untilDate().toEpochDay())
+            .addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    snackbarSender.sendError(e)
+                    return@addSnapshotListener
+                }
+
+                if (snapshot != null) {
+                    val records = snapshot.documents
+                        .mapNotNull { doc -> doc.toObject<Record>()?.copy(id = doc.id) }
+                    Timber.d("Search: result size ${records.size}")
+                    flow.tryEmit(DbResult.Success(records))
+                }
+            }
+
+        return flow
+    }
 
     private fun SearchPeriod.fromDate(): LocalDate =
         when (this) {
@@ -90,12 +100,9 @@ class SearchRepo @Inject constructor(
             is SearchPeriod.Custom -> from
         }
 
-    /**
-     * @return null means until today
-     */
-    private fun SearchPeriod.untilDate(): LocalDate? =
+    private fun SearchPeriod.untilDate(): LocalDate =
         when (this) {
-            SearchPeriod.PastMonth, SearchPeriod.PastHalfYear, SearchPeriod.PastYear -> null
+            SearchPeriod.PastMonth, SearchPeriod.PastHalfYear, SearchPeriod.PastYear -> LocalDate.now()
             is SearchPeriod.Custom -> until
         }
 

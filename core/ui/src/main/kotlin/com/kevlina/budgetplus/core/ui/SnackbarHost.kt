64bf702140ec94b49f19c01edbe9cf3721cea86f
Kevin Chiu <kevin.chiu@bandlab.com>
2025-10-10 19:47:56 +0800

    migrate snackbar use case

--- core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/SnackbarHost.kt
+++ core/ui/src/main/kotlin/com/kevlina/budgetplus/core/ui/SnackbarHost.kt
@@ -1,21 +1,23 @@
 package com.kevlina.budgetplus.core.ui
 
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kevlina.budgetplus.core.common.SnackbarData
 import com.kevlina.budgetplus.core.common.SnackbarDuration
 import com.kevlina.budgetplus.core.theme.LocalAppColors
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import androidx.compose.material3.SnackbarDuration as MaterialSnackbarDuration
 import androidx.compose.material3.SnackbarHost as MaterialSnackbarHost
@@ -49,52 +51,38 @@ fun SnackbarHost(snackbarData: SnackbarData?) {
         }
     }
 
-    SwipeableSnackbarHostWrapper(hostState) {
+    val dismissSnackbarState = rememberSwipeToDismissBoxState()
+
+    SwipeToDismissBox(
+        modifier = Modifier.fillMaxWidth(),
+        state = dismissSnackbarState,
+        backgroundContent = {},
+        onDismiss = {
+            hostState.currentSnackbarData?.dismiss()
+            scope.launch {
+                delay(50) // A small delay for better animation
+                dismissSnackbarState.reset()
+            }
+        }
+    ) {
         MaterialSnackbarHost(
             hostState = hostState,
         ) { data ->
-            Snackbar(
-                snackbarData = data,
-                containerColor = LocalAppColors.current.dark,
-                contentColor = LocalAppColors.current.light,
-                actionColor = LocalAppColors.current.light,
-                actionContentColor = LocalAppColors.current.light,
-                dismissActionContentColor = LocalAppColors.current.light,
-            )
-        }
-    }
-}
-
-@Composable
-private fun SwipeableSnackbarHostWrapper(
-    state: SnackbarHostState,
-    modifier: Modifier = Modifier,
-    dismissContent: @Composable RowScope.() -> Unit,
-) {
-    val dismissSnackbarState = rememberSwipeToDismissBoxState(
-        confirmValueChange = { value ->
-            when (value) {
-                SwipeToDismissBoxValue.Settled -> false
-                SwipeToDismissBoxValue.StartToEnd, SwipeToDismissBoxValue.EndToStart -> {
-                    state.currentSnackbarData?.dismiss()
-                    true
-                }
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Snackbar(
+                    snackbarData = data,
+                    containerColor = LocalAppColors.current.dark,
+                    contentColor = LocalAppColors.current.light,
+                    actionColor = LocalAppColors.current.light,
+                    actionContentColor = LocalAppColors.current.light,
+                    dismissActionContentColor = LocalAppColors.current.light,
+                )
             }
         }
-    )
-
-    LaunchedEffect(dismissSnackbarState.currentValue) {
-        if (dismissSnackbarState.currentValue != SwipeToDismissBoxValue.Settled) {
-            dismissSnackbarState.reset()
-        }
     }
-
-    SwipeToDismissBox(
-        modifier = modifier,
-        state = dismissSnackbarState,
-        backgroundContent = {},
-        content = dismissContent,
-    )
 }
 
 @Preview

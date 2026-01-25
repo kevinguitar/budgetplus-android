9d03d6fb937e72a1c2be629282091ee1967fa375
Kevin Chiu <kevin.chiu@bandlab.com>
2026-01-25 19:51:27 +0800

    remove RecordType workaround

--- core/common/src/androidMain/kotlin/com/kevlina/budgetplus/core/common/RecordType.kt
+++ core/common/src/androidMain/kotlin/com/kevlina/budgetplus/core/common/RecordType.kt
@@ -1,9 +1,5 @@
 package com.kevlina.budgetplus.core.common
 
-import androidx.annotation.Keep
-
-// https://issuetracker.google.com/issues/358137294#comment18
-@Keep
 enum class RecordType {
     Expense, Income
 }

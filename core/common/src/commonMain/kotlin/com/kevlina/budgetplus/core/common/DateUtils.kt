f21d91d82067ba58b1265b0f399195063bcff91c
Kevin Chiu <kevin.chiu@bandlab.com>
2026-01-27 01:23:18 +0800

    return missing withCurrentTime ext

--- core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/DateUtils.kt
+++ core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/DateUtils.kt
@@ -1,9 +1,20 @@
 package com.kevlina.budgetplus.core.common
 
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atTime
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
 
 expect val LocalDate.shortFormatted: String
 
 expect val LocalDate.mediumFormatted: String
 
 expect val LocalDate.fullFormatted: String
+
+val LocalDate.withCurrentTime: Long
+    get() {
+        val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        return atTime(now.time).toInstant(TimeZone.UTC).epochSeconds
+    }

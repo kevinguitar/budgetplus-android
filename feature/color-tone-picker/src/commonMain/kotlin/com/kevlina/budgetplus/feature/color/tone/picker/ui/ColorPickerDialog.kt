da2519c7b95bb41fd6e9f0c64a06c99bab79afb8
Kevin Chiu <kevin.chiu@bandlab.com>
2026-02-08 13:44:53 +0800

    add a color preview spacer

--- feature/color-tone-picker/src/commonMain/kotlin/com/kevlina/budgetplus/feature/color/tone/picker/ui/ColorPickerDialog.kt
+++ feature/color-tone-picker/src/commonMain/kotlin/com/kevlina/budgetplus/feature/color/tone/picker/ui/ColorPickerDialog.kt
@@ -1,11 +1,14 @@
 package com.kevlina.budgetplus.feature.color.tone.picker.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.runtime.Composable
@@ -16,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -102,10 +106,23 @@ internal fun ColorPickerDialog(
                     .height(24.dp)
             )
 
-            TextField(
-                state = hexCode,
-                title = stringResource(Res.string.color_tone_color_hex_code),
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                TextField(
+                    state = hexCode,
+                    title = stringResource(Res.string.color_tone_color_hex_code),
+                    modifier = Modifier.weight(1F),
+                )
+
+                Spacer(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .clip(AppTheme.cardShape)
+                        .background(selectedColor.color)
+                )
+            }
 
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp)

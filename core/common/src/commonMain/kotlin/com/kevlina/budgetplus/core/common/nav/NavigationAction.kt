24f82f821254994acc629e7d0ce2f69ab5401d0e
Kevin Chiu <kevin.chiu@bandlab.com>
2026-02-08 11:34:11 +0800

    adjust status bar color dynamically

--- core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/nav/NavigationAction.kt
+++ core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/nav/NavigationAction.kt
@@ -2,6 +2,7 @@ package com.kevlina.budgetplus.core.common.nav
 
 import com.kevlina.budgetplus.core.common.MutableEventFlow
 
+//TODO: This should be gone, auth and welcome screen should just be part of the nav3 destination
 fun interface NavigationAction {
     fun navigate()
 }

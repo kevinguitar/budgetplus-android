8da62b9a84a0d0c5bd2996b1d085553df313ea09
Kevin Chiu <kevin.chiu@bandlab.com>
2026-08-02 12:03:20 +0800

    Support deeplink with custom scheme (#753)
    * Support deeplink with custom scheme
    
    * add test cases

--- core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/nav/NavConstants.kt
+++ core/common/src/commonMain/kotlin/com/kevlina/budgetplus/core/common/nav/NavConstants.kt
@@ -1,6 +1,16 @@
 package com.kevlina.budgetplus.core.common.nav
 
-const val APP_DEEPLINK = "https://budgetplus.cchi.tw"
+const val APP_DEEPLINK_HOST = "budgetplus.cchi.tw"
+const val APP_DEEPLINK = "https://$APP_DEEPLINK_HOST"
+
+const val APP_DEEPLINK_SCHEME = "budgetplus"
+const val APP_DEEPLINK_CUSTOM = "$APP_DEEPLINK_SCHEME://"
+
+/**
+ * The list of accepted deeplink prefixes. Supports both the universal/app link
+ * (https://budgetplus.cchi.tw/...) and the custom scheme (budgetplus://...).
+ */
+val APP_DEEPLINK_PREFIXES = listOf(APP_DEEPLINK, APP_DEEPLINK_CUSTOM)
 
 const val NAV_RECORD_PATH = "record"
 const val NAV_JOIN_PATH = "join"

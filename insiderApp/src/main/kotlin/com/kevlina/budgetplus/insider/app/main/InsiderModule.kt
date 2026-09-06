d095e19dc0fff27c88ba01e6e6bd7b4d2db5e2b6
Kevin Chiu <kevin.chiu@bandlab.com>
2026-09-06 08:18:14 +0800

    Fix insider

--- insiderApp/src/main/kotlin/com/kevlina/budgetplus/insider/app/main/InsiderModule.kt
+++ insiderApp/src/main/kotlin/com/kevlina/budgetplus/insider/app/main/InsiderModule.kt
@@ -1,8 +1,10 @@
 package com.kevlina.budgetplus.insider.app.main
 
+import android.content.Context
 import com.kevlina.budgetplus.core.common.nav.InsiderDest
 import com.kevlina.budgetplus.core.common.nav.NavController
 import com.kevlina.budgetplus.insiderApp.BuildConfig
+import com.kevlina.budgetplus.insiderApp.R
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Named
@@ -31,4 +33,10 @@ interface InsiderModule {
     @Provides
     @Named("is_debug")
     fun provideIsDebug(): Boolean = BuildConfig.DEBUG
+
+    @Provides
+    @Named("google_client_id")
+    fun provideGoogleClientId(context: Context): String {
+        return context.getString(R.string.google_cloud_client_id)
+    }
 }
